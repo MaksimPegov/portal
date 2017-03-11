7feb6055633c9e979caca476ee0322de3d130ff1
MaksimPegov <maksim.peg@gmail.com>
2017-03-12 00:04:06 +0200

    вход/рег через email , facebook, google, twitter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBkjiMuF3cCet8-sixMu546Orsw72zRssc",
+    authDomain: "schoolportal-dd443.firebaseapp.com",
+    databaseURL: "https://schoolportal-dd443.firebaseio.com",
+    storageBucket: "schoolportal-dd443.appspot.com",
+    messagingSenderId: "17265980589"
+  };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
